55897264f22154fb8fd678eda33bf12544b7d443
Bhargava Prabu Reddy <prabu0reddy777@gmail.com>
2022-11-19 20:27:59 -0600

    Added Social Media icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Typography from "@mui/material/Typography";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import PersonIcon from "@mui/icons-material/Person";
 import Table from "./Table";
 import Flex from "./component/Flex";
 
@@ -14,7 +17,24 @@ const App = () => {
     >
       <Typography variant="h5">Internal Data Creation Tool</Typography>
       <Table />
-      <Typography variant="p">Built by Bhargava Prabu Reddy</Typography>
+      <Flex sx={{ gap: "10px" }}>
+        <Typography variant="p">Built by Bhargava Prabu Reddy</Typography>
+        <a target="_blank" href="https://www.linkedin.com/in/prabureddy/">
+          <abbr title="LinkedIn">
+            <LinkedInIcon />
+          </abbr>
+        </a>
+        <a target="_blank" href="https://github.com/prabureddy/">
+          <abbr title="Github">
+            <GitHubIcon />
+          </abbr>
+        </a>
+        <a target="_blank" href="https://prabureddy.com">
+          <abbr title="Portfolio - Personal Website">
+            <PersonIcon />
+          </abbr>
+        </a>
+      </Flex>
     </Flex>
   );
 };
